788ab43c89d950932454fd02ad3853c9186dc45b
Rasoolghr <73438580+Rasoolghr@users.noreply.github.com>
2026-09-23 10:35:37 +0330

    Update test_tsetmc.py

--- test_tsetmc.py
+++ test_tsetmc.py
@@ -1,21 +1,19 @@
 import requests
 
-URL = "https://cdn.tsetmc.com/api/Instrument/GetInstrumentSearch/خودرو"
+URL = "https://webgw.tse.ir/InstrumentProvider/api/v1/MarketWatch/MarketWatchOption/fa"
 
 headers = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/131.0 Safari/537.36"
-    )
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
 }
 
 try:
     r = requests.get(URL, headers=headers, timeout=20)
 
     print("HTTP STATUS:", r.status_code)
+    print("CONTENT LENGTH:", len(r.text))
     print("RESPONSE:")
-    print(r.text[:2000])
+    print(r.text[:5000])
 
     r.raise_for_status()
 
